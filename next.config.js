bb4f9a9a5fbe074865fe77b1c2783aa6a1b3579e
memmer27 <memmer27@gmail.com>
2026-02-26 23:26:10 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
 
   // Force Next to build as a normal server-rendered app (not static export)
-  output: "standalone",
+
 };
 
 export default nextConfig;
